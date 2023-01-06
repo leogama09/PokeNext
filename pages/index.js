10cba1f45e58a50bcf8f5e081c55a55571d54c58
Leonardo Gama <leogama09@gmail.com>
2023-01-06 14:27:26 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,34 @@
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
-  return <h1>PokeNext</h1>
+export async function getStaticProps() {
+
+  const maxPokemons = 251
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPokemons}`)
+  const data = await res.json()
+
+  // add pokemon index
+  data.results.forEach((item, index) => {
+    item.id = index + 1
+  })
+
+  return {
+    props: {
+      pokemons: data.results,
+    },
+  }
+}
+
+export default function Home({ pokemons }) {
+  return (
+    <div>
+      <h1>PokeNext</h1>
+      <ul>
+        {pokemons.map((pokemon) => (
+          <li key={pokemon.id}>{pokemon.name}</li>
+        ))}
+      </ul>
+    </div>
+  )
 }
