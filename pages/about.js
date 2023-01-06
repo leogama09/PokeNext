092da9d4e2f4c3d1b9b9c86072a2f8c97b05d4bd
Leonardo Gama <leogama09@gmail.com>
2023-01-06 18:49:12 -0300

    Few fixings / Project finished

--- pages/about.js
+++ pages/about.js
@@ -6,8 +6,12 @@ export default function About() {
     return (
         <div className={styles.about}>
             <h1>Sobre o projeto</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum laborum iure libero ex facilis, sed, animi magni dolor aspernatur odit adipisci asperiores hic veritatis rerum vitae, placeat eos quae incidunt!</p>
-            <Image src="/images/charizard.png" width="300" height="300" alt="Charizard" />
+            <p>PokeNext é um App construído em Next.js para consultar Pokémons.</p>
+            <Image 
+                src="/images/charizard.png" 
+                width="300" 
+                height="300" 
+                alt="Charizard" />
         </div>
     )
 }
