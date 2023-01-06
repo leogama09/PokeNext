4afe795628cccbfb0db3dd5719b35c2b117967a5
Leonardo Gama <leogama09@gmail.com>
2023-01-06 14:44:33 -0300

    Home CSS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import styles from '../styles/Home.module.css'
 
+import Image from 'next/image'
+
 export async function getStaticProps() {
 
   const maxPokemons = 251
@@ -22,13 +24,21 @@ export async function getStaticProps() {
 
 export default function Home({ pokemons }) {
   return (
-    <div>
-      <h1>PokeNext</h1>
-      <ul>
+    <>
+      <div className={styles.title_container}>
+        <h1 className={styles.title}>Poke<span>Next</span></h1>
+        <Image 
+          src="/images/pokeball.png" 
+          width="50" 
+          height="50" 
+          alt="PokeNext" 
+        />
+      </div>
+      <div className={styles.pokemon_container}>
         {pokemons.map((pokemon) => (
-          <li key={pokemon.id}>{pokemon.name}</li>
+          <p key={pokemon.id}>{pokemon.name}</p>
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   )
 }
